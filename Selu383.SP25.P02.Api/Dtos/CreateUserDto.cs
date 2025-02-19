d572ffb45f4fded5aa388103573c37a4b9f135fd
Subeen9 <subin.bista@selu.edu>
2025-02-19 15:27:03 -0600

    Added the Dtos

--- Selu383.SP25.P02.Api/Dtos/CreateUserDto.cs
+++ Selu383.SP25.P02.Api/Dtos/CreateUserDto.cs
@@ -2,5 +2,9 @@
 {
     public class CreateUserDto
     {
+        
+        public string UserName { get; set; } 
+        public string[] Roles { get; set; }
+        public string Password { get; set; }
     }
 }
--- Selu383.SP25.P02.Api/Dtos/UserDto.cs
+++ Selu383.SP25.P02.Api/Dtos/UserDto.cs
@@ -2,5 +2,8 @@
 {
     public class UserDto
     {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string[] Roles { get; set; }
     }
 }
