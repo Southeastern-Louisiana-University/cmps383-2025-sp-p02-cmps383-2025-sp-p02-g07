e20010ad5a6c78d9ea0596358fc5078a23097a3e
chschlich <cohen.schlicher@selu.edu>
2025-02-18 20:30:36 -0600

    additional dtos
    adds a Login, CreateUser, and User Dto, and modifies the TheaterDto

---
+++ Selu383.SP25.P02.Api/Features/Users/CreateUserDto.cs
@@ -0,0 +1,9 @@
+namespace Selu383.SP25.P02.Api.Features.Users
+{
+	public class CreateUserDto
+	{
+		public required string UserName { get; set; }
+		public required string Password { get; set; }
+		public required string[] Roles { get; set; }
+	}
+}
